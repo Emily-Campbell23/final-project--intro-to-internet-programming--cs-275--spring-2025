3a5e7d8ec3550a468ec82f3c08a4c9f370fcd2e2
Emily-Campbell23 <emcampbel@hartford.edu>
2025-04-29 10:15:57 -0400

    implemented gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,106 @@
+const { src, dest, series, watch } = require(`gulp`),
+    htmlValidator = require(`gulp-html`),
+    htmlCompressor = require(`gulp-htmlmin`),
+    cssValidator = require(`gulp-stylelint`),
+    jsLinter = require(`gulp-eslint`),
+    babel = require(`gulp-babel`),
+    jsCompressor = require(`gulp-uglify`),
+    cssCompressor = require(`gulp-clean-css`),
+    browserSync = require(`browser-sync`),
+    reload = browserSync.reload;
+
+let validateHTML = () => {
+    return src(`app/html/index.html`)
+        .pipe(htmlValidator());
+};
+
+let compressHTML = () => {
+    return src(`app/html/index.html`)
+        .pipe(htmlCompressor({collapseWhitespace:true}))
+        .pipe(dest(`prod`));
+};
+
+let validateJS = () => {
+    return src(`app/js/app.js`)
+        .pipe(jsLinter())
+        .pipe(jsLinter.formatEach(`compact`));
+};
+
+let validateCSS = () => {
+    return src(`app/css/style.css`)
+        .pipe(cssValidator({
+            failAfterError: false,
+            reporters: [{
+                formatter: `string`,
+                console: true
+            }]
+        }));
+};
+
+let compressCSS = () => {
+    return src(`app/css/style.css`)
+        .pipe(cssCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let transpileJSForDev = () => {
+    return src(`app/js/app.js`)
+        .pipe(babel())
+        .pipe(dest(`temp/js`));
+};
+
+let transpileJSForProd = () => {
+    return src(`app/js/app.js`)
+        .pipe(babel())
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+
+let compressJS = () => {
+    return src(`temp/js/app.js`)
+        .pipe(jsCompressor())
+        .pipe(dest(`prod/js`));
+};
+let serve = () => {
+    browserSync({
+        notify: true,
+        reloadDelay: 50,
+        server: {
+            baseDir: [
+                `temp`,
+                `app`,
+                `app/html`
+            ]
+        }
+    });
+    watch(`app/js/app.js`, series(jsLinter, transpileJSForDev))
+        .on(`change`, reload);
+
+    watch(`app/css/style.css`, compressCSS)
+        .on(`change`, reload);
+
+    watch(`app/html/index.html`, validateHTML)
+        .on(`change`, reload);
+};
+
+exports.default = serve;
+exports.validateHTML = validateHTML;
+exports.compressHTML = compressHTML;
+exports.validateCSS = validateCSS;
+exports.compressCSS = compressCSS;
+exports.validateJS = validateJS;
+exports.compressJS = compressJS;
+exports.transpileJSForDev = transpileJSForDev;
+exports.transpileJSForProd = transpileJSForProd;
+exports.serve = series(
+    validateHTML,
+    validateCSS,
+    transpileJSForDev,
+    serve
+);
+exports.build = series(
+    compressHTML,
+    compressCSS,
+    transpileJSForProd,
+    compressJS
+);
